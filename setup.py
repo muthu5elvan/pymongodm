165ec220514e0ea955c258d80ac1665d6887a977
Alberto Galera <galerajimenez@gmail.com>
2018-04-27 08:57:54 +0200

    support pip 10

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@
 
 import os
 import sys
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 install_reqs = parse_requirements("requirements.txt", session=False)
 try:
